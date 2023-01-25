f2aae61fe548392c6d836833db1dd1d8e80f07a9
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-01-25 16:36:08 +0200

    start

---
+++ src/routes.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+from src import api
+
+
+class Smoke(Resource):
+    def get(self):
+        return {'message': 'Ok'}, 200
+
+api.add_resource(Smoke, '/smoke', strict_slashes=False)
